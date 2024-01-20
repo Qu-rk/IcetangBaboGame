f8dfd544949046a5793d4e51a8c5f67ea9cfa48a
Qu-rk <question.mark.qu.rk@gmail.com>
2024-01-20 22:09:09 +0900

    마우스로 움직임 ㄱㄴ

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 const square = document.querySelector("#Icetang");
+square.addEventListener("mousemove", function (e){
+    square.style.left = e.clientX + "px";
+    square.style.top = e.clientY + "px";
+});
