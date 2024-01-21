3e5e1da078a102bd20b95e2b7f9097dbe0271740
Qu-rk <question.mark.qu.rk@gmail.com>
2024-01-21 19:41:19 +0900

    벽에 막힘

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const square = document.querySelector("#Icetang");
 
 let squarePositionX = 300;
-let squarePositionY = 300;
+let squarePositionY = 200;
 
 let keyWPressed = false;
 let keyAPressed = false;
@@ -11,7 +11,7 @@ let keyDPressed = false;
 let squareSpeed = 1;
 let squareXYMoveSpeed = 1;
 
-//마우스로 움직임(주석)
+// 마우스로 움직임(주석)
 // document.addEventListener("mousemove", (e) => {
 //    square.style.left = e.clientX + "px";
 //    square.style.top = e.clientY + "px";
@@ -22,15 +22,15 @@ document.addEventListener("keydown", (e) => {
         keyWPressed = true;
     }
 
-     if (e.key == "a" || e.key == "A") {
+    if (e.key == "a" || e.key == "A") {
         keyAPressed = true;
     }
 
-     if (e.key == "s" || e.key == "S") {
+    if (e.key == "s" || e.key == "S") {
         keySPressed = true;
     }
 
-     if (e.key == "d" || e.key == "D") {
+    if (e.key == "d" || e.key == "D") {
         keyDPressed = true;
     }
 })
@@ -40,15 +40,15 @@ document.addEventListener("keyup", (e) => {
         keyWPressed = false;
     }
 
-     if (e.key == "a" || e.key == "A") {
+    if (e.key == "a" || e.key == "A") {
         keyAPressed = false;
     }
 
-     if (e.key == "s" || e.key == "S") {
+    if (e.key == "s" || e.key == "S") {
         keySPressed = false;
     }
 
-     if (e.key == "d" || e.key == "D") {
+    if (e.key == "d" || e.key == "D") {
         keyDPressed = false;
     }
 })
@@ -58,16 +58,28 @@ setInterval(function() {
         squarePositionX -= squareXYMoveSpeed;
     }
 
-     if (keyAPressed) {
+    if (keyAPressed) {
         squarePositionY -= squareXYMoveSpeed;
     }
 
-     if (keySPressed) {
+    if (keySPressed) {
         squarePositionX += squareXYMoveSpeed;
     }
 
-     if (keyDPressed) {
+    if (keyDPressed) {
         squarePositionY += squareXYMoveSpeed;
+    }
+
+    if (squarePositionY <= document.querySelector("#background").style.left - 5) {
+        squarePositionY += squareXYMoveSpeed;
+    } else if (squarePositionY >= document.querySelector("#background").style.left + 885) {
+        squarePositionY -= squareXYMoveSpeed;
+    }
+
+    if (squarePositionX <= -5) {
+        squarePositionX += squareXYMoveSpeed;
+    } else if (squarePositionX >= 485) {
+        squarePositionX -= squareXYMoveSpeed;
     }
 
     square.style.top = squarePositionX + "px";
